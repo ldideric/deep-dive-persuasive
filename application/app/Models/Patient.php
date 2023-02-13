35c2405b8e22d07b80c59338728da93c36498ee2
Thijs Meijer <t.meijer@ifresh.nl>
2023-02-13 13:09:16 +0100

    PERSUASIVE-xx add patient model

---
+++ application/app/Models/Patient.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'patient_id',
+        'email',
+        'address',
+        'data'
+    ];
+
+    protected $casts = [
+        'address' => 'json',
+        'data' => 'json'
+    ];
+}
