1135f99c94ccf4fd20ae97bfadc769dd0b5b0b45
Kian Acquoij <kian.acquoij@student.rocva.nl>
2023-02-15 15:38:42 +0100

    Added icon to recent activity

--- application/resources/js/Pages/Dashboard.vue
+++ application/resources/js/Pages/Dashboard.vue
@@ -43,22 +43,26 @@ export default {
                             </div>
                         </div>
                         <div class="right bg-gray-100 w-1/3 m-3 flex flex-col items-center py-2">
-                            <div class="text-2xl font-semibold border-b-2 mb-2">Recent activity</div>
+                            <div class="text-2xl font-semibold border-b-2 border-gray-200 mb-2">Recent activity</div>
                             <div class="">
                                 <table v-if="Object.keys(results).length > 0">
                                     <tr>
                                         <th class="border-b-2 border-gray-600 p-1">Patient</th>
                                         <th class="border-b-2 border-gray-600 p-1">Activity</th>
+                                        <th class="border-b-2 border-gray-600 p-1"></th>
                                     </tr>
                                     <tr v-for="result in results" class="text-center">
-                                        <td class="border-t-2 border-gray-400 p-1 px-4">
-                                            <button @click="view(result)">
-                                                {{ result.patient.name }}
-                                            </button>
+                                        <td class="border-t-2 border-gray-400 p-1 px-4" >
+                                            {{ result.patient.name }}
+                                        </td>
+                                        <td class="border-t-2 border-gray-400 p-1 px-4 font-semibold">
+                                            New result
                                         </td>
                                         <td class="border-t-2 border-gray-400 p-1 px-4 font-semibold">
                                             <button @click="view(result)">
-                                                New result
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+                                                </svg>
                                             </button>
                                         </td>
                                     </tr>
