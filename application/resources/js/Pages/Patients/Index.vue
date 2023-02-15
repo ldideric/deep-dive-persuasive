83be4485802137e3786c4a87062b69e132782ceb
Thijs Meijer <t.meijer@ifresh.nl>
2023-02-15 15:04:28 +0100

    fix icon bug

--- application/resources/js/Pages/Patients/Index.vue
+++ application/resources/js/Pages/Patients/Index.vue
@@ -18,13 +18,6 @@
                             type="text" name="search" id="search"
                             placeholder="Search by name or ID"
                             class="block w-full rounded-md border-gray-300 pr-12 shadow-sm focus:border-blue-400 focus:ring-blue-400 sm:text-sm">
-                        <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" class="w-6 h-6 text-slate-400">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
-                            </svg>
-                        </div>
                     </div>
                 </div>
                 <div class="py-6 w-full">
